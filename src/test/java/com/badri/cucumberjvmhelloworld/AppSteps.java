0afd63da6e0f8e1ab78f52092c7c5e9128301572
badrisugavanam <badris@192.168.2.5>
2014-02-21 13:38:27 +0530

    Code formatted

--- src/test/java/com/badri/cucumberjvmhelloworld/AppSteps.java
+++ src/test/java/com/badri/cucumberjvmhelloworld/AppSteps.java
@@ -8,27 +8,25 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import org.testng.Assert;
 
-
 /**
  * Cucumber JVM adding two numbers with TestNG NOT Junit
+ *
  * @author badrisugavanam
  */
-
 public class AppSteps {
-    App app= new App();
-    int result=0;
- 
+
+    App app = new App();
+    int result = 0;
+
     @Given("(-?[0-9]+) , (-?[0-9]+)  numbers$")
-public void _numbers(int arg1, int arg2) throws Throwable {
-    app.a=arg1;
-    app.b=arg2;
-    result=app.add(arg1, arg2);
-}
-    
-@Then("^The result should be (-?[0-9]+)$")
-public void The_result_should_be(int arg3) throws Throwable {
-    Assert.assertTrue(arg3==result);
-}
+    public void _numbers(int arg1, int arg2) throws Throwable {
+        app.a = arg1;
+        app.b = arg2;
+        result = app.add(arg1, arg2);
+    }
 
-
+    @Then("^The result should be (-?[0-9]+)$")
+    public void The_result_should_be(int arg3) throws Throwable {
+        Assert.assertTrue(arg3 == result);
+    }
 }
